d6995110277f6ec75a5c812ae6d12cb49bd0929d
Behab Patnaik <behab.patnaik@gmail.com>
2025-06-20 14:56:50 -0700

    MyFirstCommit

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+
+let number;
+
+number = 0;
+number = Number(number);
+console.log(number);
+
+document.getElementById('mainNumber').textContent = number;
+
+document.getElementById('IncrementButton').addEventListener('click', function(){
+    number = number + 1;
+    document.getElementById('mainNumber').textContent = number;
+});
+
+document.getElementById('ResetButton').addEventListener('click', function(){
+    number = 0;
+    document.getElementById('mainNumber').textContent = number;
+});
+
+document.getElementById('DecrementButton').addEventListener('click', function(){
+    number = number - 1;
+    document.getElementById('mainNumber').textContent = number;
+});
+
+/*
 document.getElementById('click-btn').addEventListener('click', () => {
     alert('Button clicked!');
 });
+*/
